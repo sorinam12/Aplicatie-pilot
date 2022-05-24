e42b03d1e74ac99edebdc63d5cda25eec1ebbf30
sorinam12 <98848690+sorinam12@users.noreply.github.com>
2022-05-24 15:21:52 +0300

    UnitTest

---
+++ AplicatieTests/UnitTest1.cs
@@ -0,0 +1,95 @@
+﻿using Aplicatie;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace AplicatieTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public Pilot ExtractInputsPilot(string first, string last, string number, string localitate)
+        {
+            return new Pilot { IDPilot = 0, FirstName = first, LastName = last, NrLicense = number, Locality = localitate };
+        }
+
+        public Flight ExtractInputsFlight(string name, string TakeOffD, string TakeOffA, string LandingD, string LandingA, string emargencyA)
+        {
+            return new Flight { IDFlight = 0, Name = name, TakeOffDate = TakeOffD, TakeOffAirport = TakeOffA, LandingDate = LandingD, LandingAirport = LandingA, EmargencyAirport = emargencyA };
+        }
+
+        [TestMethod]
+        public void TestMethod_1()
+        {
+            Pilot PilotForm = new Pilot();
+            var model = ExtractInputsPilot("FirstName", "LastName", "Number", "NewYork");
+            Assert.AreEqual(model.FirstName,"FirstName");
+        }
+        [TestMethod]
+        public void TestMethod_2()
+        {
+            Pilot PilotForm = new Pilot();
+            var model = ExtractInputsPilot("FirstName", "LastName", "Number", "NewYork");
+            Assert.AreEqual(model.LastName, "LastName");
+        }
+        [TestMethod]
+        public void TestMethod_3()
+        {
+            Pilot PilotForm = new Pilot();
+            var model = ExtractInputsPilot("FirstName", "LastName", "Number", "NewYork");
+            Assert.AreEqual(model.NrLicense, "Number");
+        }
+
+        [TestMethod]
+        public void TestMethod_4()
+        {
+            Pilot PilotForm = new Pilot();
+            var model = ExtractInputsPilot("FirstName", "LastName", "Number", "NewYork");
+            Assert.AreEqual(model.Locality, "NewYork");
+        }
+
+        [TestMethod]
+        public void TestMethod_5()
+        {
+            Flight FlightForm = new Flight();
+            var model = ExtractInputsFlight("Name", "TakeOffDate", "Ghimbav", "Curand", "Sibiu", "Brasov");
+                Assert.AreEqual(model.Name, "Name");
+        }
+
+        [TestMethod]
+        public void TestMethod_6()
+        {
+            Flight FlightForm = new Flight();
+            var model = ExtractInputsFlight("Name", "TakeOffDate", "Ghimbav", "Curand", "Sibiu", "Brasov");
+                Assert.AreEqual(model.TakeOffDate, "TakeOffDate");
+        }
+
+        [TestMethod]
+        public void TestMethod_7()
+        {
+            Flight FlightForm = new Flight();
+            var model = ExtractInputsFlight("Name", "TakeOffDate", "Ghimbav", "Curand", "Sibiu", "Brasov");
+            Assert.AreEqual(model.TakeOffAirport, "Ghimbav");
+        }
+        [TestMethod]
+        public void TestMethod_8()
+        {
+            Flight FlightForm = new Flight();
+            var model = ExtractInputsFlight("Name", "TakeOffDate", "Ghimbav", "Curand", "Sibiu", "Brasov");
+            Assert.AreEqual(model.LandingDate, "Curand");
+        }
+        [TestMethod]
+        public void TestMethod_9()
+        {
+            Flight FlightForm = new Flight();
+            var model = ExtractInputsFlight("Name", "TakeOffDate", "Ghimbav", "Curand", "Sibiu", "Brasov");
+            Assert.AreEqual(model.LandingAirport, "Sibiu");
+        }
+        [TestMethod]
+        public void TestMethod_10()
+        {
+            Flight FlightForm = new Flight();
+            var model = ExtractInputsFlight("Name", "TakeOffDate", "Ghimbav", "Curand", "Sibiu", "Brasov");
+            Assert.AreEqual(model.EmargencyAirport, "Brasov");
+        }
+    }
+}
